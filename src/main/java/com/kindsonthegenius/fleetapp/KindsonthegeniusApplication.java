8ec8408f5ef47dd8c0e3723e86c0b049c0386969
Alaa <alaa@hs-merseburg.org>
2023-05-15 19:41:30 +0200

    maven

---
+++ src/main/java/com/kindsonthegenius/fleetapp/KindsonthegeniusApplication.java
@@ -0,0 +1,13 @@
+package com.kindsonthegenius.fleetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindsonthegeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindsonthegeniusApplication.class, args);
+	}
+
+}
